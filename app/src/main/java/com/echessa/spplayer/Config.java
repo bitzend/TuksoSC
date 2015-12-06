c2a9d0e09e7b2390cfbf21ca8b22f77c727314a1
Douglas Osborn <dz@bitzend.net>
2015-12-06 14:41:28 -0700

    Update Config.java
    Add Client  ID

--- app/src/main/java/com/echessa/spplayer/Config.java
+++ app/src/main/java/com/echessa/spplayer/Config.java
@@ -1,10 +1,11 @@
-package com.echessa.spplayer;
+package com.bitzend.tuksosc;
 
 /**
  * Created by echessa on 6/17/15.
+ * Update  dosborn 12/6/15
  */
 public class Config {
 
-    public static final String CLIENT_ID = "YOUR_CLIENT_ID";
+    public static final String CLIENT_ID = "0928a59108bd5ded62786955e780000c";
     public static final String API_URL = "https://api.soundcloud.com";
 }
